ecb0ec74dc2d7202771fb996d5a9a4f3b11d4afa
ericKwon95 <ericyong95@gmail.com>
2025-01-09 20:15:48 +0900

    feat: initial commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by 권승용 on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
